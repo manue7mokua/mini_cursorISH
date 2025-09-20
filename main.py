5eeb25003e7795f5f374c3b96ae9a5a2d82db048
manue7mokua <manue7codes@gmail.com>
2025-09-20 18:54:08 -0400

    Add (--verbose) flag to print additional user prompt details

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
+user_terminal_prompt = sys.argv[1]
+toggle_verbose = sys.argv[-1]
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_terminal_prompt)]),
+]
 
 def generate_some_content(user_prompt):
     if not user_prompt:
@@ -14,14 +21,15 @@ def generate_some_content(user_prompt):
         sys.exit(1)
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents={user_prompt[0]}
+        contents=messages
     )
+    if toggle_verbose == '--verbose':
+        print(f'User prompt: {user_prompt}')
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
     print(response.text)
-    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
     
 def main():
-    user_terminal_prompt = sys.argv[1:]
     generate_some_content(user_terminal_prompt)
     
 if __name__ == "__main__":
